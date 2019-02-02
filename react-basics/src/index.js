e45a53fd30fee47c8b91bbd103c1968998580a3c
Matheus Campanha <matheuscampanhaf@gmail.com>
2019-02-02 01:09:35 -0200

    initial commit

---
+++ react-basics/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// components
+import Header from './components/header'
+
+const App = () => {
+    return (
+        <div>a</div>
+    )
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
